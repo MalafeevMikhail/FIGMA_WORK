86e7def12a2f126d5184faa4ed43153dd950239b
Малафеев Михаил <mi.a.malafeev@labmedia.su>
2024-06-04 13:12:38 +0300

    task_slider_fix_problem

--- task_slider/js/second_page_script.js
+++ task_slider/js/second_page_script.js
@@ -1,67 +1,64 @@
-var counter_slider = 1;
+// Код для проверки просмотра всех слайдов и хранения номера салйда для каждого слайдар
+var ContentSlider = document.querySelectorAll('.block-slider')
+var NumberSlideInAllSlider = Array(ContentSlider.length).fill(0)
+var CheckViewAllSlideInSliders = Array()
 
-function make_circles(){
-    // Код для создания "крошек" каждый раз при переключении слайдера
-    block_dots = document.querySelector('.block-slider__dots')
+for (let index = 0; index < ContentSlider.length; index++) {
+    CountSlideInCurrentSliders = ContentSlider[index].querySelectorAll('.block-slider__item').length
+    CheckViewAllSlideInSliders.push(Array(CountSlideInCurrentSliders).fill(false))
+    CheckViewAllSlideInSliders[index][0] = true
+}
+
+function make_circles(indexSliderCurrent){
+    block_dots = ContentSlider[indexSliderCurrent].querySelector('.block-slider__dots')
+    LenghtSlideInCurrentSlider =  ContentSlider[indexSliderCurrent].querySelectorAll('.block-slider__item').length;
     block_dots.innerHTML = "";
-    for (let index = 1; index <=4; index++) {
+    for (let index = 0; index < LenghtSlideInCurrentSlider; index++) {
         dot = document.createElement('span');
         dot.classList.add('dot')
-        dot.onclick = function(){special_open_slide(index)}
-        if(index === counter_slider)
+        dot.onclick = function(){open_new_slide(indexSliderCurrent,"open_special",index)}
+        if(index === NumberSlideInAllSlider[indexSliderCurrent])
             dot.classList.add('dot_active')
         block_dots.appendChild(dot)
-
-                
     }
 }
-
-document.addEventListener("DOMContentLoaded", make_circles);
-
-function view_slide_current(){
-    //new_slide = document.querySelector('item_slider_'+counter_slider)
-    document.querySelector('.block-slider__item[data-id=slider_'+counter_slider+']').classList.add('block-slider__item_current_slide')
-    make_circles()
-}
-function special_open_slide(special_id)
-{
-    document.querySelector('.block-slider__item[data-id=slider_'+counter_slider+']').classList.remove('block-slider__item_current_slide')
-    counter_slider = special_id
-    view_slide_current()
-}
-function prev_slide_open(){
-    counter_slider-=1;
-    if (counter_slider<1){
-        document.querySelector('.block-slider__item[data-id=slider_'+(counter_slider+1)+']').classList.remove('block-slider__item_current_slide')
-        counter_slider =4
-        view_slide_current() 
+function open_new_slide(indexSliderCurrent,doName,numberOpenSlide=null){
+    /*
+        indexSliderCurrent - id слайдера
+        indexSlide - id слайда в слайдере
+        doName - действие, которое требуется сделать(предыдущий,следующий, выьранный слайд)
+     */
+  
+    LenghtSlideInCurrentSlider =  ContentSlider[indexSliderCurrent].querySelectorAll('.block-slider__item').length;
+    NumberOldSlide = NumberSlideInAllSlider[indexSliderCurrent]
+    if(doName=='prev_slide'){
+        NumberSlideInAllSlider[indexSliderCurrent]-=1
+        if(NumberSlideInAllSlider[indexSliderCurrent]<0)
+            NumberSlideInAllSlider[indexSliderCurrent]=LenghtSlideInCurrentSlider-1
     }
-    else
-    {
-        document.querySelector('.block-slider__item[data-id=slider_'+(counter_slider+1)+']').classList.remove('block-slider__item_current_slide')
-        view_slide_current()
-
+    else if (doName =='next_slide'){
+        NumberSlideInAllSlider[indexSliderCurrent]+=1
+        if(NumberSlideInAllSlider[indexSliderCurrent]>=LenghtSlideInCurrentSlider)
+            NumberSlideInAllSlider[indexSliderCurrent]=0
     }
+    else{
+        NumberSlideInAllSlider[indexSliderCurrent] = numberOpenSlide
+    }
+    CheckViewAllSlideInSliders[indexSliderCurrent][NumberSlideInAllSlider[indexSliderCurrent]]=true;
+    if(!CheckViewAllSlideInSliders[indexSliderCurrent].includes(false)){
+        ContentSlider[indexSliderCurrent].querySelector('.button-continue__button').classList.remove('button-continue__button-not-active')
+    }
+    ContentSlider[indexSliderCurrent].querySelectorAll('.block-slider__item')[NumberOldSlide].classList.remove('block-slider__item_current_slide')
+    ContentSlider[indexSliderCurrent].querySelectorAll('.block-slider__item')[NumberSlideInAllSlider[indexSliderCurrent]].classList.add('block-slider__item_current_slide')
+    make_circles(indexSliderCurrent)
 }
-
-function next_slide_open(){
-    if(counter_slider==3){
-        document.querySelector('.button-continue__button').classList.remove('button-continue__button-not-active')
-    }
-    counter_slider+=1;
-    if (counter_slider>4){
-        document.querySelector('.block-slider__item[data-id=slider_'+(counter_slider-1)+']').classList.remove('block-slider__item_current_slide')
-        counter_slider =1
+document.addEventListener('DOMContentLoaded',function(){
+    ContentSlider = document.querySelectorAll('.block-slider')
+    for (let index = 0; index < ContentSlider.length; index++) {
+        ContentSlider[index].querySelector('.block-slider__prev-button').addEventListener('click', ()=>open_new_slide(index,'prev_slide'))
+        ContentSlider[index].querySelector('.block-slider__next-button').addEventListener('click', ()=>open_new_slide(index,'next_slide'))
+        make_circles(index)
         
-        view_slide_current() 
     }
-    else
-    {
-        document.querySelector('.block-slider__item[data-id=slider_'+(counter_slider-1)+']').classList.remove('block-slider__item_current_slide')
-        view_slide_current()
-    }
-}
+})
 
-
-document.querySelector('.block-slider__prev-button').addEventListener('click',prev_slide_open)
-document.querySelector('.block-slider__next-button').addEventListener('click',next_slide_open)
