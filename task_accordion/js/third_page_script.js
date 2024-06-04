8083abdb314d3ce501bf668b7c69898638606465
Малафеев Михаил <mi.a.malafeev@labmedia.su>
2024-06-04 13:56:16 +0300

    task_accordion_fix_problem

--- task_accordion/js/third_page_script.js
+++ task_accordion/js/third_page_script.js
@@ -1,32 +1,57 @@
-var count_tab = 3
-var tabsArr = Array(count_tab).fill(0)
-function check_all_view()
-{
-    for (let index = 0; index < tabsArr.length; index++) {
-        if(tabsArr[index]===0){
-            return false
-        }
-    }
-    return true
+var ContentAccordion = document.querySelectorAll('.main-content')
+
+var CheckViewAllAccordItem = Array()
+
+for (let index = 0; index < ContentAccordion.length; index++) {
+    CountSlideInCurrentSliders = ContentAccordion[index].querySelectorAll('.main-content__tab-item').length
+    CheckViewAllAccordItem.push(Array(CountSlideInCurrentSliders).fill(false))
+    CheckViewAllAccordItem[index][0] = true
 }
-function open_items(special_id){
+
+// var tabsArr = Array(count_tab).fill(0)
+// function check_all_view()
+// {
+//     for (let index = 0; index < tabsArr.length; index++) {
+//         if(tabsArr[index]===0){
+//             return false
+//         }
+//     }
+//     return true
+// }
+function open_items(indexTabContent,indexTabItem){
         // var cur_element =  document.getElementById('tab_'+index).classList.toggle('tab_active')
-        var cur_element =  document.querySelector('.main-content__tab-content[data-id=tab_content_'+special_id+']')
+        var cur_element =  ContentAccordion[indexTabContent].querySelectorAll('.main-content__tab-content')[indexTabItem]
 
         console.log(cur_element)
+        CheckViewAllAccordItem[indexTabContent][indexTabItem] = true
+
+        if(!CheckViewAllAccordItem[indexTabContent].includes(false))
+            ContentAccordion[indexTabContent].querySelector('.button-continue__button').classList.remove('button-continue__button-not-active')
         if (cur_element.classList.contains('main-content__tab-item_active')){
-            document.querySelector('.main-content__tab-icon[data-id=icon_'+special_id+']').setAttribute('src','./img/btn_down_click.svg')
+            ContentAccordion[indexTabContent].querySelectorAll('.main-content__tab-icon')[indexTabItem].setAttribute('src','./img/btn_down_click.svg')
 
         }
         else
         {
-            document.querySelector('.main-content__tab-icon[data-id=icon_'+special_id+']').setAttribute('src','./img/btn_up_click.svg')
+            ContentAccordion[indexTabContent].querySelectorAll('.main-content__tab-icon')[indexTabItem].setAttribute('src','./img/btn_up_click.svg')
 
         }
         cur_element.classList.toggle('main-content__tab-item_active')
-        document.querySelector('.main-content__tab-item[data-id=tab_'+special_id+']').classList.toggle('main-content__tab-item_open')
-        tabsArr[special_id-1] = 1
-        console.log(tabsArr)
-        if(check_all_view()==true)
-            document.querySelector('.button-continue__button').classList.remove('button-continue__button-not-active')
+        ContentAccordion[indexTabContent].querySelectorAll('.main-content__tab-item')[indexTabItem].classList.toggle('main-content__tab-item_open')
+        //tabsArr[special_id-1] = 1
+        // if(check_all_view()==true)
+        //     document.querySelector('.button-continue__button').classList.remove('button-continue__button-not-active')
 }
+
+
+document.addEventListener('DOMContentLoaded',function(){
+    allElementsTabContent = document.querySelectorAll('.main-content') 
+    //console.log("всего - ", allElementsTabContent)  
+    for (let index = 0; index < allElementsTabContent.length; index++) {
+        allElementTabMenu = allElementsTabContent[index].querySelectorAll('.main-content__name-tab')
+        //console.log("Внутри - ", allElementTabMenu)
+        for (let j_index = 0; j_index < allElementTabMenu.length; j_index++) {
+            allElementTabMenu[j_index].addEventListener('click',()=>open_items(index,j_index))
+        }
+    }
+})
